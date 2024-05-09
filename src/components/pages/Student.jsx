59915ea4ece32b1e0f9b67095deaccfbdf4ff1ad
rajaeziki <rajaeziki35@gmail.com>
2024-05-09 20:31:31 +0100

    ziki commit

--- src/components/pages/Student.jsx
+++ src/components/pages/Student.jsx
@@ -2,15 +2,8 @@ import React from "react";
 import "./Student.css"
 export const Student = () => {
     return(
-        <div >
-        <header>
-          <h1>Welcome to Our Website</h1>
-        </header>
-       
-        
-        <footer>
-          <p>&copy; {new Date().getFullYear()} Real Time . Application.</p>
-        </footer>
+        <div  className="">
+      
       </div>
     );
 }
